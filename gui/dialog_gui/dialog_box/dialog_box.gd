9087df4272b5981489448352d9e8f89a24f0a049
Darenn <keller.darenn@gmail.com>
2020-04-15 16:39:43 +0200

    Fixed the artifacts in dialog boxes.

--- gui/dialog_gui/dialog_box/dialog_box.gd
+++ gui/dialog_gui/dialog_box/dialog_box.gd
@@ -18,7 +18,8 @@ export(int) var text_margin = 60
 # When sentence is under 10 characters, will use this size as label.rect_size.x
 export(int) var font_character_width = 29
 
-onready var _starting_size_x = self.rect_size.x
+onready var _starting_size_x
+onready var _starting_size_y
 onready var _label: RichTextLabel = $Background/RichTextLabel
 onready var _label_tween: Tween = $Background/RichTextLabel/Tween 
 
@@ -26,6 +27,7 @@ func _ready():
 	# Need to wait another frame so that rect_size is properly update in engine
 	yield(get_tree(), "idle_frame")
 	_starting_size_x = self.rect_size.x
+	_starting_size_y = self.rect_size.y
 #	display_text("MY NAME I")
 #	display_text("Hello there! How do you feel today? I feel very great. \n" +
 #	"I managed to change the dialog box size dynamically relative to the text size. " +
@@ -37,33 +39,27 @@ func clear():
 	
 func _process(delta) -> void:
 	pass
-	# Need to wait 1 frame so that the text is drawn, 
-	# and that we can get  the content height
-	# make it in process so that the size get adjusted in case of screen resize
-	_adjust_size()
 
 # Adjust the size of the dialog box relative to its text content
 func _adjust_size() -> void:
-	if _label.get_visible_line_count() == 1:
-		rect_size.x = 	_label.get_font("normal_font").get_string_size(_label.text).x + 50	
-	self.rect_size.y = _label.get_content_height()
-	_label.margin_top = _label.get_content_height()/2
+	self.rect_size.y = _label.get_content_height() * 1.2
+	if _label.get_visible_line_count() <= 1:
+		rect_size.x = _label.get_font("normal_font").get_string_size(_label.text).x * 1.2
 
 # Display the given text at speed character per second
-# TODO: I got a strange bug, the end of the text was written above the last line! 
-# Maybe a problem of size when looking at the content height? Mayby scrolling ?
-# Ok seems like very long word make the bug
-func display_text(text: String, speed: int = 20) -> void:
-	visible = false # make it invisible so that we can use the draw sizes
+func display_text(text: String, speed: int = 25) -> void:
+	_label_tween.stop_all()
+	_label.visible_characters = -1
 	self.rect_size.x = _starting_size_x
+	self.rect_size.y = _starting_size_y
 	_label.clear()
-	_label.bbcode_text = ""
 	_label.push_align(RichTextLabel.ALIGN_CENTER)
 	_label.append_bbcode(text)
-
-	yield(get_tree(), "idle_frame")
-	visible = true # after one frame the size should be o
+	# VERY DIRTY but label properties are updated at drawing...
+	propagate_notification(NOTIFICATION_DRAW)
+	_adjust_size()
+	_label.visible_characters = 0
 	
 	_label_tween.interpolate_property(_label, "visible_characters", 0, text.length(),
-			text.length() / speed, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			text.length() / speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	_label_tween.start()
